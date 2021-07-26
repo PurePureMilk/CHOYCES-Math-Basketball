c3c2fc2fb3d1d5b9a61722eaef528c10769c26b4
Knightling23 <49211912+Knightling23@users.noreply.github.com>
2021-07-26 16:39:48 -0400

    Add files via upload

---
+++ Scenes/LevelSelectorscript.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectorscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+
+    public void goback()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void add1()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void add2()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void add3()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void add4()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void add5()
+    {
+        SceneManager.LoadScene(8);
+    }
+   
+    //////////////////////////////////////////////
+    
+
+
+
+    public void sub1()
+    {
+        SceneManager.LoadScene(9);
+    }
+    public void sub2()
+    {
+        SceneManager.LoadScene(10);
+    }
+    public void sub3()
+    {
+        SceneManager.LoadScene(11);
+    }
+    public void sub4()
+    {
+        SceneManager.LoadScene(12);
+    }
+    public void sub5()
+    {
+        SceneManager.LoadScene(13);
+    }
+
+
+    //////////////////////////////////////////////
+
+
+
+    public void multi1()
+    {
+        SceneManager.LoadScene(14);
+    }
+    public void multi2()
+    {
+        SceneManager.LoadScene(15);
+    }
+    public void multi3()
+    {
+        SceneManager.LoadScene(16);
+    }
+    public void multi4()
+    {
+        SceneManager.LoadScene(17);
+    }
+    public void multi5()
+    {
+        SceneManager.LoadScene(18);
+    }
+
+
+    //////////////////////////////////////////////
+
+
+
+    public void Div1()
+    {
+        SceneManager.LoadScene(19);
+    }
+    public void Div2()
+    {
+        SceneManager.LoadScene(20);
+    }
+    public void Div3()
+    {
+        SceneManager.LoadScene(21);
+    }
+    public void Div4()
+    {
+        SceneManager.LoadScene(22);
+    }
+    public void Div5()
+    {
+        SceneManager.LoadScene(23);
+    }
+
+}
