3c4b6ece72948968c2773c3d89f62abbef644df9
Knightling23 <49211912+Knightling23@users.noreply.github.com>
2021-07-26 16:44:36 -0400

    Add files via upload

---
+++ Settings/Fullscreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fullscreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void fullscreens(bool is_fullscene)
+        {
+        Screen.fullScreen =is_fullscene;
+
+        }
+}
